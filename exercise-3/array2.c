107186200ff7a1581e71d9cf11360ed478f10fca
Gwen <72200363+agthgwn@users.noreply.github.com>
2023-09-15 22:25:10 +0800

    Update array2.c

--- exercise-3/array2.c
+++ exercise-3/array2.c
@@ -5,21 +5,17 @@
 
 #include<stdio.h>
 #include<conio.h>
-
 main()
 {
 	int a[11], b[10], n;
 	
 	printf("Enter 11 numbers: ");
-	for(n = 0; n < 11; n++)
-	{
+	for(n = 0; n < 11; n++) {
 		scanf("%d", &a[n]);
 	}
 	
-	
 	printf("\nSUM of TWO succeeding pairs: ");
-	for(n = 0; n < 10; n++)
-	{
+	for(n = 0; n < 10; n++)	{
 		b[n] = a[n] + a[n+1];
 		printf("%d ", b[n]);	
 	}
