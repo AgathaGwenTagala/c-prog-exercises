963ecd74da8d768f9953c77ce4f2030c04f2c6d9
Gwen <72200363+agthgwn@users.noreply.github.com>
2023-09-15 22:26:51 +0800

    Update and rename Exercise 3-3.c to array3.c

--- exercise-3/array3.c
+++ exercise-3/array3.c
@@ -1,8 +1,8 @@
-/*PROGRAMMING EXERCISE 3-3
+/*ARRAY EXERCISE 3
   An instructor gives a multiple choice test which consists of 5 questions, each of which is answered by a choice of one 
-  of the numbers one to five. The program should grade the test. If the student choice is 1 give 1 point. 
-  If the choice is 2, give 2 points. 3 points if choice answer is 3 and 4 pints for answer 4 and choice 5 is 5 points. 
-  Otherwise no point is given to other choices. Print out the average score of the student in the test.
+  of the numbers one to five. The program should grade the test. If the student's choice is, 1 give 1 point. 
+  If the choice is 2, give 2 points. 3 points if the choice answer is 3 and 4 points for answer 4 and choice 5 is 5 points. 
+  Otherwise, no point is given to other choices. Print out the average score of the student on the test.
 */
 
 #include<stdio.h>
@@ -17,8 +17,7 @@ main()
 	printf("Enter student name: ");
 	scanf("%s", &name);
 	
-	for(n = 0; n < 5; n++)
-	{
+	for(n = 0; n < 5; n++)	{
 		printf("Enter Answer #%d? ", n+1);
 		scanf("%d", &a[n]);
 		if (a[n] <= 5) 
