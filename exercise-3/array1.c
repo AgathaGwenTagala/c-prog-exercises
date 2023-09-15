2233bae4976a1da41fae2aed59be5074b86066da
Gwen <72200363+agthgwn@users.noreply.github.com>
2023-09-15 22:23:47 +0800

    Update and rename Exercise 3-1.c to array1.c

--- exercise-3/array1.c
+++ exercise-3/array1.c
@@ -1,9 +1,9 @@
-/*PROGRAMMING EXERCISE 3-1
+/*ARRAY EXERCISE 1
   Write a program to take two numerical lists of the same length ended with sentinel value and store the lists in 
   array X and array Y, each of which has 20 elements. Let N be the actual number of data values in each list. Store 
   the product of corresponding elements of array X and array Y into third array Z of size 20. Display the square root 
   of the sum of the items in array Z. Make up your own data and be sure to test your program on at least one data set 
-  with number exactly 20 items.
+  with number of exactly 20 items.
 */
 
 #include<stdio.h>
